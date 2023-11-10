6382f9e51945db5a1da532534d4a4e02dff91b1a
dittolk <135496711+dittolk@users.noreply.github.com>
2023-11-10 17:26:56 +0700

    MIN-27 Added routes, added add cashier page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import ManagerCashier from "./components/manageCashier/manageCashier";
+import ManageProduct from "./components/manageProduct";
+import Required from "./components/required";
+import AdminDashboard from "./pages/adminDashboard";
+import Home from "./pages/home";
+import WelcomePage from "./pages/welcomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <WelcomePage></WelcomePage>},
+  { path: "/home", element: <Home></Home>},
+  { path: "/admin-dashboard", element: <AdminDashboard></AdminDashboard>},
+  { path: "/manage-cashier", element: <ManagerCashier></ManagerCashier>},
+  { path: "/manage-product", element: <ManageProduct></ManageProduct>},
+  {
+    element: <Required></Required>,
+    children: [
+      //untuk yang butuh token
+    ],
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }

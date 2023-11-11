4e3645934a33feba40a0e9ff5063b28316e7538f
Hafizh24 <hafizhcbs@gmail.com>
2023-11-11 10:03:22 +0700

    MIN-30 update card profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import CardProfile from "../components/profile/CardProfile";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  image: Yup.mixed().test("fileSize", "File size must be less than 1MB", (value) => {
+    if (value) {
+      return value.size <= 1145728;
+    }
+    return true;
+  }),
+});
 
 const Profile = () => {
+  const formik = useFormik({
+    initialValues: {
+      image: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, action) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <>
       <CardProfile />

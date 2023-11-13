010730281ddc1ca54483fe24cd15fdb60cac6a24
dittolk <135496711+dittolk@users.noreply.github.com>
2023-11-13 23:08:23 +0700

    MIN 27 (#6)
    * MIN-27 added authorization admin features etc
    
    * MIN-27 added authorization admin features etc
    
    * MIN-27 Added routes, added add cashier page (#3)
    
    * Min 30 (#5)
    
    * MIN-30 update card profile
    
    * MIN-30 add validation
    
    * MIN-27 added authorization admin features etc
    
    * MIN-27 added authorization admin features etc
    
    * MIN-27 Added routes, added add cashier page (#3)
    
    * Min 30 (#5)
    
    * MIN-30 update card profile
    
    * MIN-30 add validation
    
    * MIN-27 added authorization admin features etc
    
    * MIN-27 added authorization admin features etc
    
    * MIN-27 Added routes, added add cashier page (#3)
    
    * Min 30 (#5)
    
    * MIN-30 update card profile
    
    * MIN-30 add validation
    
    ---------
    
    Co-authored-by: hafizh khairuddin <95872736+Hafizh24@users.noreply.github.com>

---
+++ client/src/components/manageCashier/subcomponents/addCashier.jsx
@@ -0,0 +1,111 @@
+import {
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Button,
+    Heading,
+    useColorModeValue,
+    Text,
+    useToast,
+  } from '@chakra-ui/react'
+import { useFormik } from "formik";
+import axios from "axios";
+import * as Yup from "yup"
+
+function AddCashier({getCashierData}){
+    const toast = useToast();
+    const token = localStorage.getItem('token')
+
+    const handleSubmit = async (data) => {
+        try{
+            await axios.post("http://localhost:2000/user/add-user", data, {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                }
+            }); //sending data to database
+            getCashierData();
+            toast({
+                title: "Success", description: `Cashier with username : ${data.username} has been created`, status: "success", duration: 3000, position: "top",
+            });
+        }catch(err){
+            console.log(err.response.data.message);
+            toast({
+                title: "Error", description: `${data.username} already exist`, status: "error", duration: 3000, position: "top",
+            });
+        }
+    }
+
+    const RegisterEventSchema = Yup.object().shape({
+        username: Yup.string().required("Username can't be empty"),
+        email: Yup.string().email("Must be a valid email format").required("Email can't be empty"),
+        password: Yup.string().min(3, "Must be at least 3 characters long").required("Password can't be empty"),
+      });
+
+    const formik = useFormik({
+        initialValues:{
+          username: "",
+          email: "",
+          password: "",
+        },
+        validationSchema: RegisterEventSchema,
+        onSubmit:(values, action) => {
+            handleSubmit(values)
+            action.resetForm();
+        }
+      })
+    return(
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} minW={'32vw'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Add cashier</Heading>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <form onSubmit={formik.handleSubmit}>
+                <Stack spacing={7}>
+                <FormControl>
+                    <FormLabel>Username</FormLabel>
+                    <Input name='username' value={formik.values.username} onChange={formik.handleChange} type="text" focusBorderColor='#3C6255' autoComplete='off' rounded={'full'} _focus={{backgroundColor: '#3C6255', color:'white'}} 
+                    error={formik.touched.username && Boolean(formik.errors.username)}/>
+                    {formik.touched.username && formik.errors.username ? (
+                        <Text mt={2} style={{color: 'red'}}>{formik.errors.username}</Text>
+                    ) : null}
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Email address</FormLabel>
+                    <Input name='email' value={formik.values.email} onChange={formik.handleChange} type="email" focusBorderColor='#3C6255' autoComplete='new' rounded={'full'} _focus={{backgroundColor: '#3C6255', color:'white'}} error={formik.touched.email && Boolean(formik.errors.email)}/>
+                    {formik.touched.email && formik.errors.email ? (
+                        <Text mt={2} style={{color: 'red'}}>{formik.errors.email}</Text>
+                    ) : null}
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <Input name='password' value={formik.values.password} onChange={formik.handleChange} type="password" focusBorderColor='#3C6255' autoComplete='off' rounded={'full'} _focus={{backgroundColor: '#3C6255', color:'white'}} error={formik.touched.password && Boolean(formik.errors.password)}/>
+                    {formik.touched.password && formik.errors.password ? (
+                        <Text mt={2} style={{color: 'red'}}>{formik.errors.password}</Text>
+                    ) : null}
+                </FormControl>
+                <Stack>
+                    <Button
+                    type='submit'
+                    bg={'#3C6255'}
+                    color={'white'}
+                    _hover={{
+                        bg: '#61876E',
+                    }}
+                    rounded={'full'}>
+                    Create
+                    </Button>
+                </Stack>
+                </Stack>
+            </form>
+          </Box>
+        </Stack>
+    )
+}
+
+export default AddCashier;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,14 +1,35 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Home() {
-    return(
-        <>
-            <Link to={'/admin-dashboard'}>
-                <Button bgColor={'#3C6255'} _hover={{backgroundColor:'#61876E'}} color={'white'}>Admin Dashboard</Button>
-            </Link>
-        </>
-    )
+  const user = useSelector((state) => state.user.value);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+  };
+  return (
+    <>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        minH={"100vh"}
+        direction={"column"}>
+        <Text>Hello {user.username}</Text>
+        <Link to={"/admin-dashboard"}>
+          <Button bgColor={"#3C6255"} _hover={{ backgroundColor: "#61876E" }} color={"white"}>
+            Admin Dashboard
+          </Button>
+        </Link>
+        <Button
+          onClick={() => {
+            handleLogout();
+          }}>
+          Logout
+        </Button>
+      </Flex>
+    </>
+  );
 }
 
 export default Home;

0e86946ec98cd62904aca41b15d7d502bb7c03de
Hafizh24 <hafizhcbs@gmail.com>
2023-11-26 18:37:34 +0700

    update cart and sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -20,20 +20,20 @@ import SalesReport from './pages/SalesReport'
 import ProductSales from './pages/ProductSales'
 
 const router = createBrowserRouter([
-  { path: '/', element: <WelcomePage></WelcomePage> },
-  { path: '/login-admin', element: <LoginAdmin></LoginAdmin> },
-  { path: '/login-cashier', element: <LoginCashier></LoginCashier> },
-  { path: '/verify/:token', element: <Verify></Verify> },
-  { path: '/reset-password/:token', element: <ResetPasswordPage></ResetPasswordPage> },
+  { path: '/', element: <WelcomePage /> },
+  { path: '/login-admin', element: <LoginAdmin /> },
+  { path: '/login-cashier', element: <LoginCashier /> },
+  { path: '/verify/:token', element: <Verify /> },
+  { path: '/reset-password/:token', element: <ResetPasswordPage /> },
 
   {
     element: <Required />,
     children: [
       //untuk yang butuh token
-      { path: '/home', element: <Home></Home> },
-      { path: '/manage-cashier', element: <ManagerCashier></ManagerCashier> },
-      { path: '/manage-product', element: <ManageProduct></ManageProduct> },
-      { path: '/admin-settings', element: <AdminSettings></AdminSettings> },
+      { path: '/home', element: <Home /> },
+      { path: '/manage-cashier', element: <ManagerCashier /> },
+      { path: '/manage-product', element: <ManageProduct /> },
+      { path: '/admin-settings', element: <AdminSettings /> },
       { path: '/profile', element: <Profile /> },
       { path: '/manage-category', element: <ManageCategory /> },
       { path: '/sales-report', element: <SalesReport /> },
@@ -50,9 +50,7 @@ function App() {
   const keepLogin = async () => {
     try {
       const response = await axios.get('http://localhost:2000/users/keep-login', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
+        headers: { Authorization: `Bearer ${token}` }
       })
       dispatch(setData(response.data.user))
     } catch (err) {
@@ -62,6 +60,7 @@ function App() {
 
   useEffect(() => {
     keepLogin()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
     <>
--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -15,36 +15,25 @@ import {
   ModalFooter,
   useToast,
   ModalCloseButton,
-  Image,
-  Skeleton,
+  Image
 } from '@chakra-ui/react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../redux/cartSlice'
 import ModalUpdateProduct from './manageProduct/subcomponents/modalUpdateProduct'
 import { DeleteIcon, EditIcon, WarningIcon } from '@chakra-ui/icons'
 import axios from 'axios'
-import { useState } from 'react'
-import { addToCart } from '../redux/cartSlice'
 
 export default function Card({ productData, getProducts }) {
-  const toast = useToast();
-  const user = useSelector((state) => state.user.value);
-  const token = localStorage.getItem("token");
-  
-  const { isOpen: isOpenUpdate, onOpen: onOpenUpdate, onClose: onCloseUpdate } = useDisclosure();
-  const { isOpen: isOpenWarning, onOpen: onOpenWarning, onClose: onCloseWarning } = useDisclosure();
-  
+  const toast = useToast()
+  const user = useSelector((state) => state.user.value)
+  const token = localStorage.getItem('token')
+
+  const { isOpen: isOpenUpdate, onOpen: onOpenUpdate, onClose: onCloseUpdate } = useDisclosure()
+  const { isOpen: isOpenWarning, onOpen: onOpenWarning, onClose: onCloseWarning } = useDisclosure()
+
   const handleDelete = async () => {
     try {
-      await axios.patch(
-        `http://localhost:2000/products/delete-product`,
-        { id: productData.id },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-      )
+      await axios.patch(`http://localhost:2000/products/delete-product`, { id: productData.id }, { headers: { Authorization: `Bearer ${token}` } })
       toast({
         title: 'Success',
         description: 'Selected product has been deleted',
@@ -81,10 +70,9 @@ export default function Card({ productData, getProducts }) {
         mx={'13px'}
         transition="transform 0.2s ease-in-out" // Smooth transition over 0.3 seconds
         _hover={{
-          transform: "scale(1.05)",  // Scale up to 105% when hovered
-        }}
-      >
-        <Box overflow={"hidden"} h={{ base: "140px", md: "140px", lg: "200px" }} bg={"gray.100"} mt={-6} mx={-6} mb={6} pos={"relative"}>          
+          transform: 'scale(1.05)' // Scale up to 105% when hovered
+        }}>
+        {/* <Box overflow={'hidden'} h={{ base: '140px', md: '140px', lg: '200px' }} bg={'gray.100'} mt={-6} mx={-6} mb={6} pos={'relative'}>
           <Image src={`http://localhost:2000/${productData.image}`} fill alt={productData?.name} />
         </Box>{' '}
         */}
@@ -94,13 +82,14 @@ export default function Card({ productData, getProducts }) {
             // eslint-disable-next-line react-hooks/rules-of-hooks
             color={'#3C6255'}
             fontSize={['xs', 'lg']}
-            fontFamily={'body'}>
+            fontFamily={'body'}
+            textTransform={'capitalize'}>
             {productData?.name}
           </Heading>
           <Text color={'black'} fontWeight={800} fontSize={['xs', 'sm']} letterSpacing={1.1}>
             Rp.{productData?.price}
           </Text>
-          <Text color={'black'} fontSize={['xs', 'sm']} letterSpacing={1.1}>
+          <Text color={'black'} noOfLines={4} fontSize={['xs', 'sm']} letterSpacing={1.1}>
             {user.isAdmin === true && <>{productData?.description}</>}
           </Text>
           <Text color={'black'} fontSize={['xs', 'sm']} letterSpacing={1.1}>
@@ -109,65 +98,55 @@ export default function Card({ productData, getProducts }) {
           <Text color={'black'} fontSize={['xs', 'sm']} letterSpacing={1.1}>
             {user.isAdmin === true && <>Status : {productData?.isActive === true ? <>Active</> : <>Inactive</>}</>}
           </Text>
-          <HStack
-            justifyContent={"space-between"}
-            divider={<StackDivider borderColor="gray.400" />}>
-            {user?.isAdmin === true ? (
-              <>
-                <Button size={["sm", "md"]} colorScheme="messenger">
-                  Edit product
-                </Button>
-              </HStack>
-            </>
+          {user?.isAdmin === true ? (
+            <HStack justifyContent={'space-evenly'}>
+              <Button
+                onClick={() => {
+                  onOpenUpdate()
+                }}
+                size={['sm', 'md']}
+                bgColor={'#3C6255'}
+                _hover={{ bg: '#61876E' }}
+                mr={2}
+                color={'white'}>
+                <EditIcon />
+              </Button>
+              <Button
+                onClick={() => {
+                  onOpenWarning()
+                }}
+                size={['sm', 'md']}
+                colorScheme="red">
+                <DeleteIcon />
+              </Button>
+            </HStack>
           ) : (
-            <>
-              <Stack justifyContent={'center'} alignItems={'center'} mt={14}>
-                <Button
-                  w={48}
-                  bgColor={'#3C6255'}
-                  _hover={{ bg: '#61876E' }}
-                  color={'white'}
-                  onClick={() => {
-                    dispatch(
-                      addToCart({ id: productData.id, quantity: 1, amount: productData.price })
-                    );
-                    toast({
-                      title: "Success",
-                      description: `${productData.name} has been added to cart`,
-                      status: "success",
-                      duration: 1000,
-                      position: "top",
-                    });
-                  }}
-                  size={["sm", "md"]}
-                  colorScheme="messenger">
-                  Add to cart
-                </Button>
-              </>
-            )}
-          <HStack justifyContent={'flex-end'} divider={<StackDivider borderColor="gray.400" />}>
-            {user?.isAdmin === true ? 
-            <>
-            <Button onClick={() => {onOpenUpdate()}} size={["sm", "md"]} bgColor={'#3C6255'} _hover={{bg: '#61876E'}} mr={2} color={'white'}>
-              <EditIcon/>
-            </Button>
-            <Button onClick={() => {onOpenWarning()}} size={["sm", "md"]} colorScheme="red">
-              <DeleteIcon/>
-            </Button>
-            </> 
-            : 
-            <>
-            </>}
-          </HStack>
+            <Stack justifyContent={'center'} alignItems={'center'} mt={14}>
+              <Button
+                w={48}
+                isDisabled={productData.total_stock <= 0 ? true : false}
+                bgColor={'first'}
+                _hover={{ bg: 'third' }}
+                color={'white'}
+                onClick={() => {
+                  dispatch(addToCart({ id: productData.id, quantity: 1, amount: productData.price }))
+                  toast({
+                    title: 'Success',
+                    description: `${productData.name} has been added to cart`,
+                    status: 'success',
+                    duration: 1000,
+                    position: 'top'
+                  })
+                }}
+                size={['sm', 'md']}>
+                {productData.total_stock <= 0 ? 'out of stock' : 'Add to cart'}
+              </Button>
+            </Stack>
+          )}
         </Stack>
       </Box>
 
-      <ModalUpdateProduct
-      isOpenUpdate={isOpenUpdate}
-      onCloseUpdate={onCloseUpdate}
-      productData={productData}
-      getProducts={getProducts}
-      />
+      <ModalUpdateProduct isOpenUpdate={isOpenUpdate} onCloseUpdate={onCloseUpdate} productData={productData} getProducts={getProducts} />
 
       <Modal isOpen={isOpenWarning} onClose={onCloseWarning} isCentered>
         <ModalOverlay />
